27b73473a55a9c874145e0d8f2bb8249691f1916
Sergei Winitzki <sergei.winitzki@versal.com>
2014-04-01 13:49:19 -0700

    more adjustments to TOC formatting

--- make-md-toc.sh
+++ make-md-toc.sh
@@ -3,4 +3,4 @@
 #  From each line that begins with #s, generate an indented hyperlink
 # Strip all punctuation, replace spaces with dashes, make all lowercase.
 
-grep '^#' | perl -pe 's|^(#+) +(.*)$|$x=$1; $text=$2; $link=$text; $x =~ s/#/&nbsp;/g; $link =~ s/ /-/g;  $link =~ s/,//g; $link =~ s,[./+!?:;]+,,g; $link =~ y/A-Z/a-z/; sprintf("%s [%s](#%s)",$x,$text,$link)|e'
+grep '^#' | perl -pe 's|^(#+) +(.*)$|$hashes=$1; $indent=$hashes; $text=$2; $link=$text; $indent =~ s/^#//; $indent =~ s/#/....../g; if ($hashes =~ /^#$/) { $text="**$text**"; }; $link =~ s/ /-/g;  $link =~ s/,//g; $link =~ s,[./+!?:;]+,,g; $link =~ y/A-Z/a-z/; sprintf("%s [%s](#%s)<br/>",$indent,$text,$link)|e'
